db17e4703b4981d34e562183b03a89ec2352c901
3musketeersoftware <ysarslanturk@gmail.com>
2023-07-11 15:33:48 +0300

    Proje dosyası ekle.

---
+++ OOP/TasitKrediManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class TasitKrediManager : IKrediManager
+    {
+        public void BiseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("Taşıt kredisi ödeme planı hesaplandı");
+        }
+    }
+}
